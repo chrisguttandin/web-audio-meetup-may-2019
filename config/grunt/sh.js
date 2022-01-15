cdd200627710fe793931c7e635b055a5f172f232
Christoph Guttandin <chrisguttandin@media-codings.com>
2022-01-15 23:15:46 +0100

    build(config): update grunt config

--- config/grunt/sh.js
+++ config/grunt/sh.js
@@ -5,11 +5,11 @@ module.exports = (grunt) => {
 
     return {
         'analyze': {
-            cmd: `npx ng build --prod --source-map --stats-json && \
+            cmd: `npx ng build --configuration production --source-map --stats-json && \
                 webpack-bundle-analyzer build/web-audio-meetup-may-2019/stats.json`
         },
         'build': {
-            cmd: 'npx ng build --base-href /web-audio-meetup-may-2019/ --prod --subresource-integrity'
+            cmd: 'npx ng build --base-href /web-audio-meetup-may-2019/ --configuration production --subresource-integrity'
         },
         'continuous': {
             cmd: 'npx ng test'
@@ -36,7 +36,7 @@ module.exports = (grunt) => {
             cmd: 'npx ng run web-audio-meetup-may-2019:server:production && npx angular-prerender --preserve-index-html'
         },
         'preview': {
-            cmd: 'npx ng serve --prod'
+            cmd: 'npx ng serve --configuration production'
         },
         'smoke': {
             cmd: env.CI
