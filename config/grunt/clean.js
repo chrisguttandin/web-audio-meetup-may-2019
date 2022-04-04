2b9a2012b70e7aa729c5c32fc5ba8f6f88dc0ac0
Christoph Guttandin <chrisguttandin@media-codings.com>
2022-04-05 00:45:15 +0200

    build(config): update build config

--- config/grunt/clean.js
+++ config/grunt/clean.js
@@ -1,6 +1,4 @@
 module.exports = {
     'runtime': ['build/web-audio-meetup-may-2019/runtime*.js'],
-    'scripts': ['build/web-audio-meetup-may-2019/!(ngsw-worker).js'],
-    'source-maps': ['build/web-audio-meetup-may-2019/**.map'],
-    'styles': ['build/web-audio-meetup-may-2019/**.css']
+    'source-maps': ['build/web-audio-meetup-may-2019/**.map']
 };
